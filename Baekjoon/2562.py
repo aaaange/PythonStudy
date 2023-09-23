22b5d07fb4b4833194030fd4e2378b9084a7362a
akrsoTale <phantom1208@naver.com>
2023-09-23 15:40:56 +0900

    [2562] solve with akrsoTale

--- Baekjoon/2562.py
+++ Baekjoon/2562.py
@@ -13,4 +13,32 @@
 # 출력
 # 첫째 줄에 최댓값을 출력하고, 둘째 줄에 최댓값이 몇 번째 수인지를 출력한다.
 
+# 10개의 자연수의 구분이 엔터임.
+# input = input() # 한 줄, 즉 엔터를 기점으로 한 줄을 가져오는 함수
 
+
+
+# when
+listData = []
+for i in range(10):
+    listData.append(int(input()))
+    
+# print(listData)
+
+
+
+# then
+def is_i_greaterthen_j(i, j) -> bool:
+    return i > j
+max = 0 # 최댓값
+line = 0 # 해당 데이터가 있던 줄
+
+# [13123, 232324, 23555, 21323, 1, 23, 3, 4, 256634, 2]
+for index, data in enumerate(listData): # enumerate(데이터) -> 인덱스와 데이터를 같이 반환해줌 
+    if data > max:
+        max = data
+        line = index + 1
+
+# finally
+print(max)
+print(line)
