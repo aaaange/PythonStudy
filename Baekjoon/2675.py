80e9b889df6bea19ed22c660ee9823ddea4bc8b6
aaaange <ayung00@naver.com>
2024-07-17 08:47:43 +0900

    [Study] BaekJoon 2675 풀이 완료

--- Baekjoon/2675.py
+++ Baekjoon/2675.py
@@ -12,3 +12,38 @@
 # 출력
 # 각 테스트 케이스에 대해 P를 출력한다.
 
+test_cace = int(input())
+# 두번 째 줄에 들어오는 입력값을 공백을 기준으로 나누고 각 변수에 따로 넣는다. 그리고 리스트 0번은 숫자로 변경해주고(r) 1번은 그대로 둔다.(s)
+# 그리고 r 숫자만큼 s의 각 글자에 곱해서 출력해주기.
+# 이를 test_cace만큼 반복한다.
+
+
+# for i in range(test_cace):
+#     list_1 = input().split()
+#     # repeat, string = input().split()
+#     s = len(string)
+    
+#     for j in range(s):
+#         output_1 = []
+#         output_1 = string[j]*int(repeat)
+#         print(''.join(output_1))
+#     print(output_1)
+#     i += 1
+
+# for i in range(test_cace):
+#     list_1 = input().split()
+#     s = len(list_1[1])
+    
+#     for j in range(s):
+#         output_1 = []
+#         output_1 = list_1[1][j]*int(list_1[0])
+#         print(output_1, end="")
+#     i += 1
+
+
+for i in range(test_cace):
+    repeat, char = input().split()
+    for i in range(len(char)):
+        print(char[i]*int(repeat), end='')
+    print()
+
