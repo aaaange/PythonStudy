90921759e64dc2a265d8f82ffe738f41200c0d83
aaaange <ayung00@naver.com>
2024-09-02 21:51:21 +0900

    [Silver II] Baekjoon 1874 스택 수열 - 정답

--- Baekjoon/1874.py
+++ Baekjoon/1874.py
@@ -13,39 +13,33 @@
 # 출력
 # 입력된 수열을 만들기 위해 필요한 연산을 한 줄에 한 개씩 출력한다. push연산은 +로, pop 연산은 -로 표현하도록 한다. 불가능한 경우 NO를 출력한다.
 
-N = int(input()) # 수열 개수
+def stack_sequence():
+    N = int(input().strip())
+    sequence = [int(input().strip()) for _ in range(N)]
+    # print(sequence)
 
-arr = []
+    stack = []
+    result = []
+    current = 1
 
-# 수열 입력
-for i in range(N):
-    arr.append(int(input()))
+    # 목표 수열의 각 숫자에 대해 반복
+    for num in sequence:
+        # 목표 숫자(num)까지의 숫자를 스택에 푸시
+        while current <= num:
+            stack.append(current)
+            result.append('+')
+            current += 1
+        
+        # 스택의 상단 숫자가 목표 숫자(num)와 일치하는 경우, 팝
+        if stack and stack[-1] == num:
+            stack.pop()
+            result.append('-')
+        else:
+            # 스택의 상단 숫자가 목표 숫자(num)와 일치하지 않는 경우, 수열을 만들 수 없으므로 "NO"를 출력하고 종료
+            print("NO")
+            return
 
-print(arr)
+    # 모든 수열을 성공적으로 처리한 후, 명령어 리스트를 줄 바꿈으로 구분하여 출력
+    print("\n".join(result))
 
-stack = [0]* N
-top = -1
-result = []
-
-for idx in range(N-1, -1, -1): # 스택은 선입후출이니 뒤에서부터 봄
-    for num in range(1,N+1):
-        if arr[idx] == num:
-            if stack[top] <= num:
-                top += 1
-                stack[top] = num
-                break
-            else:
-                while stack[top] > num:
-                    result.append(stack.pop(top))
-                    top -= 1
-                top += 1
-                stack[top] = num
-                break
-
-while top > -1:
-    result.append(stack.pop(top))
-    top -= 1
-    
-print(result)
-
-## 내림차순 배열을 만드는 코드를 짜버림.. 
+stack_sequence()
