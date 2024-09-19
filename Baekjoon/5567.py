8770c8e24811bcd2aa327eadd750bef0b1585f08
aaaange <ayung00@naver.com>
2024-09-20 00:30:29 +0900

    [Silver II] Baekjoon 5567 결혼식 - 3차 제출 정답

--- Baekjoon/5567.py
+++ Baekjoon/5567.py
@@ -11,23 +11,33 @@
 # 출력
 # 첫째 줄에 상근이의 결혼식에 초대하는 동기의 수를 출력한다.
 
+from collections import deque
+
 N = int(input()) # 상근이의 동기 수
 M = int(input()) # 관계 수
-
-p = [0]*(N+1) # 0 ~ N까지 인덱스 만들어 주기
+graph = [[0] * (N+1) for _ in range(N+1)]
+visited = [0]*(N+1) # 0 ~ N까지 인덱스 만들어 주기
 count = 0
 
 for i in range(M):
     x, y = map(int, input().split())
-    if x == 1: # x가 상근이면
-        p[y] = 1 # 상근이의 친구는 1로 표시
-    elif p[x] == 1: # x가 상근이의 친구면
-        p[y] = 2 # 상근이의 친구는 2로 표시
-    elif p[y] == 1: # y가 상근이의 친구면
-        p[x] = 2 # x는 상근이 친구의 친구로 2 표시
+    graph[x].append(y)
+    graph[y].append(x)
 
+def BFS(x):
+    q = deque([x])
+    visited[x] = 1
+    
+    while q:
+        friend = q.popleft()
+        for people in graph[friend]:
+            if visited[people] == 0:
+                q.append(people)
+                visited[people] = visited[friend] + 1
+
+BFS(1)
 for idx in range(2, N+1): # 1부터 시작하는 데 1은 상근이라 셀 필요 x
-    if p[idx] == 1 or p[idx] == 2:
+    if 0< visited[idx] <=3:
         count += 1
 
 print(count)
