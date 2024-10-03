0faa22bb17961e832bd9f5c1dc54587f36606a27
aaaange <ayung00@naver.com>
2024-10-03 19:09:28 +0900

    [Silver II] Baekjoon 18111 마인크래프트 - 1차 제출 오답

--- Baekjoon/18111.py
+++ Baekjoon/18111.py
@@ -17,3 +17,51 @@
 # 출력
 # 첫째 줄에 땅을 고르는 데 걸리는 시간과 땅의 높이를 출력하시오. 답이 여러 개 있다면 그중에서 땅의 높이가 가장 높은 것을 출력하시오.
 
+N, M, B = map(int, input().split()) # N 세로 길이, M 가로 길이, B 인벤토리 블럭 개수
+map_list = [list(map(int, input().split())) for _ in range(N)]
+# print(map_list)
+h_list = []
+time = 0
+
+# 최빈값 구하기
+f_list = [0] * 257 # 높이가 0부터 256까지니까 256도 세주기 위해서
+for i in range(N):
+    for j in range(M):
+        f_list[map_list[i][j]] += 1
+        h_list.append(map_list[i][j]) # 최고값 찾으려고 미리 담아두기
+        
+mode = f_list.index(max(f_list)) # 최빈값
+
+def flatten(mode, B):
+    global h_list, time
+    if mode < 0:
+        return
+    mode_value = mode
+    # 최빈값보다 큰 수와의 차
+    for idx in range(mode,max(h_list)+1): # 최빈값부터 최대값까지만 순회시키기
+        if f_list[idx] == 0: # 값이 없으면 continue
+            continue 
+        else:
+            B += abs(mode - idx)*f_list[idx]
+            time += abs(mode - idx)*f_list[idx]*2
+
+    # 만약 최빈값이 가장 낮은 땅이었다면 함수를 끝내고, 그렇지 않다면 낮은 값을 인벤토리의 블럭으로 채워줄 수 있는지 확인
+    if mode == min(h_list):
+        return mode_value
+    else:
+        count = 0 # 채울 때 필요한 블럭 수 
+        for idx_2 in range(min(h_list),mode):
+            if f_list[idx_2] == 0:
+                continue
+            else:
+                count += (mode - idx_2)*f_list[idx_2]
+        if B >= count:
+            time += count
+            return mode_value
+        else:
+            return flatten(mode-1, B)
+
+m_v = flatten(mode, B)
+
+print(time, m_v)
+
